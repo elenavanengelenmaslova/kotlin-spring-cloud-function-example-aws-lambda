dd77e61d92ec7e6961f5f1809bd0bf7726d5b1d0
elena.vanengelen <elena.vanengelen@edsn.nl>
2023-02-14 11:04:58 +0100

    add table and x86 stack, rename arm64 stack

--- infrastructure/src/main/kotlin/com/example/infra/InfrastructureApp.kt
+++ infrastructure/src/main/kotlin/com/example/infra/InfrastructureApp.kt
@@ -12,15 +12,46 @@ fun main() {
         .region(System.getenv("DEPLOY_TARGET_REGION"))
         .build()
 
-    val stackName = "Kotlin-Lambda-Spring-Cloud-Function"
-    InfrastructureJvmArm64Stack(
-        app, stackName,
-        StackProps.builder()
-            .stackName(stackName)
+    val stackNameTable = "Kotlin-Lambda-Spring-Cloud-Function-table"
+    InfrastructureTableStack(
+        app, stackNameTable, StackProps.builder()
+            .stackName(stackNameTable)
             .env(environment)
-            .description("Kotlin Spring Cloud Function")
+            .description("Dynamo Table used for Spring-Cloud-Function example")
             .build()
     )
 
+    val stackNameJvmX86 = "Kotlin-Lambda-Spring-Cloud-Function-Jvm-X86"
+    InfrastructureJvmX86Stack(
+        app, stackNameJvmX86,
+        StackProps.builder()
+            .stackName(stackNameJvmX86)
+            .env(environment)
+            .description("Kotlin Spring Cloud Function JVM X86")
+            .build()
+    )
+
+    val stackNameJvmArm64 = "Kotlin-Lambda-Spring-Cloud-Function-Jvm-Arm64"
+    InfrastructureJvmArm64Stack(
+        app, stackNameJvmArm64,
+        StackProps.builder()
+            .stackName(stackNameJvmArm64)
+            .env(environment)
+            .description("Kotlin Spring Cloud Function JVM ARM64")
+            .build()
+    )
+
+//    val stackNameJvmSnapStart = "Kotlin-Lambda-Spring-Cloud-Function-Jvm-X86"
+//    InfrastructureJvmX86Stack(
+//        app, stackNameJvmX86,
+//        StackProps.builder()
+//            .stackName(stackNameJvmX86)
+//            .env(environment)
+//            .description("Kotlin Spring Cloud Function JVM X86")
+//            .build()
+//    )
+
+    //Native X86
+    //Native ARM 64
     app.synth()
 }
--- infrastructure/src/main/kotlin/com/example/infra/InfrastructureJvmX86Stack.kt
+++ infrastructure/src/main/kotlin/com/example/infra/InfrastructureJvmX86Stack.kt
@@ -3,22 +3,20 @@ package com.example.infra
 import software.amazon.awscdk.Duration
 import software.amazon.awscdk.Stack
 import software.amazon.awscdk.StackProps
-import software.amazon.awscdk.services.lambda.Architecture
 import software.amazon.awscdk.services.lambda.Code
 import software.amazon.awscdk.services.lambda.Function
 import software.amazon.awscdk.services.lambda.Runtime
 import software.amazon.awscdk.services.logs.RetentionDays
 import software.constructs.Construct
 
-class InfrastructureJvmArm64Stack(scope: Construct, id: String, props: StackProps) : Stack(scope, id, props) {
+class InfrastructureJvmX86Stack(scope: Construct, id: String, props: StackProps) : Stack(scope, id, props) {
     init {
-        val functionId = "lambdaStringCloudFunctionJvmARM64"
+        val functionId = "lambdaStringCloudFunctionJvmX86"
         Function.Builder.create(this, functionId)
-            .description("Kotlin Lambda Spring Cloud Function JVM ARM 64")
+            .description("Kotlin Lambda Spring Cloud Function JVM X86")
             .handler("org.springframework.cloud.function.adapter.aws.FunctionInvoker")
             .runtime(Runtime.JAVA_11)
             .code(Code.fromAsset("../build/dist/function.jar"))
-            .architecture(Architecture.ARM_64)
             .logRetention(RetentionDays.ONE_WEEK)
             .memorySize(512)
             .environment(
