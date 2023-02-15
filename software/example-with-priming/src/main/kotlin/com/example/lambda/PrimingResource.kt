5763b9a22eb6251e2bc4ece67831b11300e4fed8
elena.vanengelen <elena.vanengelen@edsn.nl>
2023-02-15 14:03:22 +0100

    fix priming

--- software/example-with-priming/src/main/kotlin/com/example/lambda/PrimingResource.kt
+++ software/example-with-priming/src/main/kotlin/com/example/lambda/PrimingResource.kt
@@ -1,14 +1,17 @@
 package com.example.lambda
 
-import com.example.lambda.controller.ProductsController
+import com.example.lambda.model.Product
+import com.example.lambda.model.ProductRequest
 import org.crac.Context
 import org.crac.Core
 import org.crac.Resource
 import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Configuration
+import org.springframework.messaging.Message
+import org.springframework.messaging.MessageHeaders
 
 @Configuration
-class PrimingResource(private val productsController: ProductsController) : Resource {
+class PrimingResource(private val requestHandler: (Message<ProductRequest>) -> Product?) : Resource {
     private val logger = LoggerFactory.getLogger(this::class.java)
 
     init {
@@ -19,7 +22,10 @@ class PrimingResource(private val productsController: ProductsController) : Reso
     override fun beforeCheckpoint(context: Context<out Resource>?) {
         logger.info("beforeCheckpoint hook")
         runCatching {
-            productsController.find("i dont exist")
+            requestHandler.invoke(object: Message<ProductRequest>{
+                override fun getPayload() = ProductRequest("i dont exist")
+                override fun getHeaders(): MessageHeaders = MessageHeaders(emptyMap())
+            })
         }
     }
 
