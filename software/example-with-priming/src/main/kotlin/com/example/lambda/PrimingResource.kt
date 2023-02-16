673777b3305fa7b671b1e867a653ba372ec8a880
elena.vanengelen <elena.vanengelen@edsn.nl>
2023-02-16 09:43:15 +0100

    add refresh app context

--- software/example-with-priming/src/main/kotlin/com/example/lambda/PrimingResource.kt
+++ software/example-with-priming/src/main/kotlin/com/example/lambda/PrimingResource.kt
@@ -7,18 +7,18 @@ import org.crac.Context
 import org.crac.Core
 import org.crac.Resource
 import org.slf4j.LoggerFactory
+import org.springframework.context.ConfigurableApplicationContext
 import org.springframework.context.annotation.Configuration
 import org.springframework.messaging.Message
 import org.springframework.messaging.MessageHeaders
 
 @Configuration
-class PrimingResource(private val requestHandler: (Message<ProductRequest>) -> Product?, private val service: ProductsService) : Resource {
+class PrimingResource(private val requestHandler: (Message<ProductRequest>) -> Product?, private val service: ProductsService, private val applicationContext: ConfigurableApplicationContext
+) : Resource {
     private val logger = LoggerFactory.getLogger(this::class.java)
 
     init {
-        logger.info("start register priming")
         Core.getGlobalContext().register(this)
-        logger.info("finished register priming")
     }
 
     override fun beforeCheckpoint(context: Context<out Resource>?) {
@@ -26,16 +26,17 @@ class PrimingResource(private val requestHandler: (Message<ProductRequest>) -> P
         kotlin.runCatching {
             service.findProduct("i dont exist")
         }
-//        runCatching {
-//            requestHandler.invoke(object: Message<ProductRequest>{
-//                override fun getPayload() = ProductRequest("1")
-//                override fun getHeaders(): MessageHeaders = MessageHeaders(emptyMap())
-//            })
-//        }
-//        logger.info("finished beforeCheckpoint hook")
+        runCatching {
+            requestHandler.invoke(object: Message<ProductRequest>{
+                override fun getPayload() = ProductRequest("1")
+                override fun getHeaders(): MessageHeaders = MessageHeaders(emptyMap())
+            })
+        }
+        logger.info("finished beforeCheckpoint hook")
     }
 
     override fun afterRestore(context: Context<out Resource>?) {
+        applicationContext.refresh()
         logger.info("afterRestore hook")
     }
 }
